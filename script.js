bf8312c84b5d79c2f28910d707e08fbe217d725d
min gyun kim <60118539+Dasari03@users.noreply.github.com>
2021-11-05 10:31:47 -0700

    Add files via upload
    Toggle for the modal to display and hide.

---
+++ script.js
@@ -0,0 +1,13 @@
+const btns = document.querySelector(".option_buttons");
+const btn = document.querySelectorAll(".options");
+const option1 = document.querySelector(".option_1");
+const option2 = document.querySelector(".option_2");
+const option3 = document.querySelector(".option_3");
+const description1 = document.querySelector(".description_1");
+const description2 = document.querySelector(".description_2");
+const description3 = document.querySelector(".description_3");
+const descriptionContationer = document.querySelector(".description_container");
+
+// option2.addEventListener("click", () => {
+//   description2.classList.toggle("display_active");
+// });
